cd7f1e6b4f9b108aeb099d0982e85a67bc2a6d2e
ReactCLass <gho.morteza@proton.me>
2026-08-01 21:10:30 +0330

    Todo

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,55 +1,150 @@
+import { LucideCheckCircle, LucidePencil, LucidePlusCircle, LucideTrash, LucideUndo } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import './App.css';
+import Button from "./components/Button";
 import Loading from './components/Loading';
+import { BASE_URL } from './constants';
 
 function App() {
 
-  const [users, setUsers] = useState([]);
+  const [todos, setTodos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [formLoading, setFormLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    title: '',
+    isCompleted: false
+  });
+  const [showError, setShowError] = useState(false);
 
 
-  const getUsers = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        console.log(data);
-        setUsers(data);
-        setIsLoading(false);
-      })
+  const getTodos = () => {
+    fetch(`${BASE_URL}/todos`)
+      .then(response => response.json())
+      .then(data => setTodos(data))
       .catch((error) => {
         console.error(error);
-        setIsLoading(false);
       })
+      .finally(() => setIsLoading(false))
   }
 
 
   useEffect(() => {
-    getUsers();
-  }, [])
+    getTodos();
+  }, []);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    setShowError(false);
+    if (!formData.title) {
+      setShowError(true);
+      return
+    }
+
+
+    setFormLoading(true);
+
+    fetch(`${BASE_URL}/todos`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        title: formData.title,
+        completed: formData.isCompleted,
+      })
+    })
+      .then((res) => {
+        console.log(res);
+        const newItems = [...todos, {title: formData.title, completed: formData.isCompleted}];
+        setTodos(newItems);
+
+        alert('Todo added successfully');
+
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setFormLoading(false))
+
+  } // handleSubmit
+
+
+
 
 
   return (
     <main className='bg-slate-900 text-gray-200 min-h-screen p-8'>
 
-      {
-        isLoading ?
-          <Loading />
-          :
-          <ul className='divide-y divide-gray-300 max-w-[300px]'>
-            {
-              users.map((user) => {
-                return (
-                  <li key={user.id} className='py-2 hover:bg-gray-800'>
-                    <p className='text-lg'>{user.name}</p>
-                    <span className='text-base text-gray-500'>{user.email}</span>
-                  </li>
-                )
-              })
-            }
-          </ul>
-      }
+      <section className='grid grid-cols-3 gap-12 max-w-2/3 mx-auto'>
+
+        <div className="col-span-1">
+          <h1 className='text-2xl font-bold mb-4'>Create Todo</h1>
+
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div className='mb-2'>
+              <label className='text-lg mb-1 flex justify-between items-baseline'>
+                Title
+                {
+                  showError &&
+                  <span className='text-xs text-red-500'>Title is Required</span>
+                }
+              </label>
+              <input
+                type="text"
+                placeholder='Enter Todo Title'
+                className='w-full border border-gray-400 bg-gray-800 px-3 py-2 text-lg rounded-md'
+                onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+              />
+            </div>
+            <div className='mb-4'>
+              <label className='text-lg flex items-center gap-1.5 cursor-pointer'>
+                <input
+                  type="checkbox"
+                  className='w-4 h-4'
+                  onChange={(e) => setFormData({ ...formData, isCompleted: e.target.checked })}
+                />
+                Completed
+              </label>
+            </div>
+            <Button type="submit" color='blue' size='lg' icon={<LucidePlusCircle size={22} />} text="Add" isLoading={formLoading} />
+          </form>
+
+        </div>
+
+        <div className='col-span-2'>
+          <h1 className='text-2xl font-bold mb-4'>Todos List</h1>
+          {
+            isLoading ?
+              <Loading />
+              :
+              <ul className='divide-y divide-gray-500 border border-gray-300 rounded-lg overflow-hidden'>
+                {
+                  todos.map((item) => {
+                    return (
+                      <li key={item.id} className='flex justify-between px-4 py-2 transition-all hover:bg-gray-800'>
+                        <p className={`text-base ${item.completed ? 'line-through opacity-50' : ''}`}>{item.title}</p>
+                        <div className='flex gap-1'>
+                          {
+                            item.completed ?
+                              <Button color="gray" size='sm' icon={<LucideUndo />} justIcon tooltip='Uncomplete' />
+                              :
+                              <Button color="green" size='sm' icon={<LucideCheckCircle />} justIcon tooltip='Completed' />
+                          }
+                          <Button color="blue" size='sm' icon={<LucidePencil />} justIcon tooltip='Edit' />
+                          <Button color="red" size='sm' icon={<LucideTrash />} justIcon tooltip='Delete' />
+                        </div>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+          }
+        </div>
+
+      </section>
+
+
 
 
     </main>
--- react-app/src/components/Button.jsx
+++ react-app/src/components/Button.jsx
@@ -1,6 +1,19 @@
 import { LoaderCircle } from 'lucide-react';
 
-const Button = ({ type, text, color = 'gray', size = 'md', className = '', icon, isLoading = false, isDisabled = false, onClick }) => {
+const Button = ({
+  type,
+  text,
+  color = 'gray',
+  size = 'md',
+  className = '',
+  icon,
+  isLoading = false,
+  isDisabled = false,
+  justIcon = false,
+  tooltip = '',
+  onClick
+}) => {
+
 
   let colorClass = '';
   let sizeClass = '';
@@ -17,11 +30,11 @@ const Button = ({ type, text, color = 'gray', size = 'md', className = '', icon,
   }
 
   switch (size) {
-    case 'lg': sizeClass = 'rounded-lg py-2 px-3 text-xl';
+    case 'lg': sizeClass = `rounded-lg py-2 px-3 text-xl ${justIcon ? 'h-10 w-10 p-0' : ''}`;
       break;
-    case 'md': sizeClass = 'rounded-md py-1 px-2 text-base';
+    case 'md': sizeClass = `rounded-md py-1 px-2 text-base ${justIcon ? 'h-8 w-8 p-0' : ''}`;
       break;
-    case 'sm': sizeClass = 'rounded-sm py-1 px-1.5 text-sm';
+    case 'sm': sizeClass = `rounded-sm py-1 px-1.5 text-sm ${justIcon ? 'h-6 w-6 p-0' : ''}`;
       break;
 
   }
@@ -32,9 +45,10 @@ const Button = ({ type, text, color = 'gray', size = 'md', className = '', icon,
       className={`cursor-pointer transition-all flex items-center gap-1 disabled:opacity-50 disabled:cursor-not-allowed! ${colorClass} ${sizeClass} ${className}`}
       onClick={onClick}
       disabled={isLoading || isDisabled ? true : false}
+      title={tooltip}
     >
       {!isLoading && icon ? icon : null}
-      {isLoading ? <LoaderCircle size={18} className='animate-spin' /> : undefined }
+      {isLoading ? <LoaderCircle size={18} className='animate-spin' /> : undefined}
       {text}
     </button>
   )
