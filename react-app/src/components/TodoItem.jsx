541c90700c3643abe474dea68acf865a95654867
ReactCLass <gho.morteza@proton.me>
2026-08-11 20:32:19 +0330

    todo list - final

---
+++ react-app/src/components/TodoItem.jsx
@@ -0,0 +1,117 @@
+import { LucideCheckCircle, LucidePencil, LucideTrash, LucideUndo } from "lucide-react";
+import { useState } from "react";
+import { BASE_URL } from "../constants";
+import Button from "./Button";
+
+const TodoItem = ({ todo, prepareToEdit, afterDelete }) => {
+
+    const [deleteLoading, setDeleteLoading] = useState(false);
+    const [statusLoading, setStatusLoading] = useState(false);
+
+    const handleDelete = () => {
+
+        if (!window.confirm('Are you sure to delete this item?')) {
+            return
+        }
+
+        setDeleteLoading(true);
+        fetch(`${BASE_URL}/todos/${todo.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+            .then((res) => {
+                console.log(res);
+
+                // getTodos()
+                afterDelete(todo.id)
+                alert('Todo deleted successfully');
+
+            })
+            .catch((error) => console.error(error))
+            .finally(() => setDeleteLoading(false))
+
+    } // handleDelete
+
+
+    const handleChangeStatus = (newStatus) => {
+
+        setStatusLoading(true);
+
+        fetch(`${BASE_URL}/todos/${todo.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify({
+                completed: newStatus,
+            })
+        })
+            .then((res) => {
+                console.log(res);
+
+                // getTodos()
+
+                // const upadtedItems = todos.map((todo) => todo.id === editingId ? { ...todo, title: formData.title, completed: formData.isCompleted } : todo);
+                // setTodos(upadtedItems)
+
+                // alert('Todo edited successfully');
+
+                todo.completed = newStatus
+
+            })
+            .catch((error) => console.error(error))
+            .finally(() => setStatusLoading(false))
+
+    } // handleUpdate
+
+    return (
+        <li className='flex justify-between px-4 py-2 transition-all hover:bg-gray-800'>
+            <p className={`text-base ${todo.completed ? 'line-through opacity-50' : ''}`}>{todo.title}</p>
+            <div className='flex gap-1'>
+                {
+                    todo.completed ?
+                        <Button
+                            color="gray"
+                            size='sm'
+                            icon={<LucideUndo />}
+                            justIcon
+                            tooltip='Uncomplete'
+                            isLoading={statusLoading}
+                            onClick={() => handleChangeStatus(false)}
+                        />
+                        :
+                        <Button
+                            color="green"
+                            size='sm'
+                            icon={<LucideCheckCircle />}
+                            justIcon
+                            tooltip='Completed'
+                            isLoading={statusLoading}
+                            onClick={() => handleChangeStatus(true)}
+                        />
+                }
+                <Button
+                    color="blue"
+                    size='sm'
+                    icon={<LucidePencil />}
+                    justIcon
+                    onClick={() => prepareToEdit(todo.id)}
+                    tooltip='Edit'
+                />
+                <Button
+                    color="red"
+                    size='sm'
+                    icon={<LucideTrash />}
+                    justIcon
+                    isLoading={deleteLoading}
+                    onClick={handleDelete}
+                    tooltip='Delete'
+                />
+            </div>
+        </li>
+    )
+}
+
+export default TodoItem;
