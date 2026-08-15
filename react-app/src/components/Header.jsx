afe0ff5ae1e9e21607f237f3f5991547b5ce561f
morteza <gho.morteza@gmail.com>
2026-08-15 13:53:06 +0330

    active link

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,14 +1,19 @@
 import { NavLink } from "react-router"
 
 const Header = () => {
+
+  const activeLink = (isActive) => {
+    return isActive ? 'text-red-500' : ''
+  }
+
   return (
     <header className='bg-slate-700 px-8 py-4 '>
       <nav>
         <ul className='flex justify-center gap-12 text-lg'>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/about-us">About Us</NavLink></li>
-          <li><NavLink to="/users">Users</NavLink></li>
-          <li><NavLink to="/posts">Posts</NavLink></li>
+          <li><NavLink to="/" className={({ isActive }) => activeLink(isActive)}>Home</NavLink></li>
+          <li><NavLink to="/about-us" className={({ isActive }) => activeLink(isActive)}>About Us</NavLink></li>
+          <li><NavLink to="/users" className={({ isActive }) => activeLink(isActive)}>Users</NavLink></li>
+          <li><NavLink to="/posts" className={({ isActive }) => activeLink(isActive)}>Posts</NavLink></li>
         </ul>
       </nav>
     </header>
