e347c268d3539e7614341a6684557fb106162866
morteza <gho.morteza@gmail.com>
2026-09-05 10:50:29 +0330

    apply some cahnges

--- react-ts/src/pages/posts/components/Create.tsx
+++ react-ts/src/pages/posts/components/Create.tsx
@@ -1,9 +1,9 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
+import { toast } from "react-hot-toast/headless";
 import DsButton from "../../../components/design-system/DsButton";
 import PageHeader from "../../../components/global/PageHeader";
 import { createPost } from "../../../services/post-service";
-import { toast } from "react-hot-toast/headless";
 
 const formDataInitial = {
   title: "",
@@ -16,20 +16,24 @@ const CreatePost = () => {
   const client = useQueryClient();
 
   const { mutate, isPending } = useMutation({
-    mutationFn: createPost
+    mutationFn: createPost,
+    onSuccess: () => {
+      toast.success("پست با موفقیت ایجاد شد");
+      setFormData(formDataInitial);
+      client.invalidateQueries({ queryKey: ['posts'] });
+    },
+    onError: () => {
+      toast.error("خطا در ایجاد پست");
+    }
   });
 
   const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     mutate({
       title: formData.title,
       body: formData.body,
       userId: 1,
     });
-    toast.success("پست با موفقیت ایجاد شد");
-    setFormData(formDataInitial);
-    client.invalidateQueries({ queryKey: ['posts'] });
   };
 
   return (
